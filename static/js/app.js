879277b0f592ca9c69b08a88eb0657c36b11ecd3
Pam Hamrick <leahamr@yahoo.com>
2020-08-11 13:48:42 -0500

    added code to handle user click then build table

--- static/js/app.js
+++ static/js/app.js
@@ -19,8 +19,32 @@ function buildTable(data) {
         let cell = row.append("td");
         //Add each value from the object into a cell
         cell.text(val);
+        
         }
       );
     });
-  }
-  
+};
+
+function handleClick() {
+    // Use d3 to select datatime tag from filter and get the value from that tag
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    
+     // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}; 
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
